51afd549e1cc5404ff37e90eb30fb5fe94b1d668
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-08-23 16:46:17 -0500

    okok, fotos de pokemon

--- src/components/TrainerRow.jsx
+++ src/components/TrainerRow.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function TrainerRow({ trainer }) {
+	const [pokemonImage, setPokemonImage] = useState("");
+
+	useEffect(() => {
+		const fetchPokemonImage = async () => {
+			try {
+				const response = await fetch(
+					`https://pokeapi.co/api/v2/pokemon/${trainer.mainPokemon.toLowerCase()}/`
+				);
+				const data = await response.json();
+				setPokemonImage(data.sprites.other.home.front_default);
+			} catch (error) {
+				console.error("Error fetching Pokémon data:", error);
+			}
+		};
+
+		fetchPokemonImage();
+	}, [trainer.mainPokemon]);
+
 	return (
 		<tr>
 			<td className="px-4 py-2 text-center">{trainer.id}</td>
@@ -14,9 +32,9 @@ function TrainerRow({ trainer }) {
 			<td className="px-4 py-2 text-center">{trainer.name}</td>
 			<td className="px-4 py-2 text-center">
 				<img
-					src={`https://img.pokemondb.net/sprites/black-white/anim/normal/${trainer.mainPokemon}.gif`}
+					src={pokemonImage}
 					alt={trainer.mainPokemon}
-					className="w-16 h-16 mx-auto"
+					className="w-20 h-20 mx-auto object-contain"
 				/>
 			</td>
 			<td className="px-4 py-2 text-center">{trainer.totalPokemon}</td>
